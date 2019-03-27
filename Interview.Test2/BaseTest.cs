043f763679309a441b14cc1430b859f99441e8d7
AustinRGovender <austinryang1@gmail.com>
2019-03-27 17:40:31 +0200

    added reporting element

---
+++ Interview.Test2/BaseTest.cs
@@ -0,0 +1,65 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.Test2
+{
+    [TestFixture]
+    public class BaseTest
+    {
+
+       public ExtentReports extent = null;
+        public ExtentTest test = null;
+
+        [OneTimeSetUp]
+        public void ExtentStart()
+        {
+            //Start operation to build test report
+            extent = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"C:\Users\User\Desktop\New folder\Interview\Interview.Test2\Reports\TestReport.html");
+            extent.AttachReporter(htmlReporter);
+        }
+
+        [OneTimeTearDown]
+        public void ExtentClose()
+        {
+            extent.Flush();
+        }
+
+        [SetUp]
+        public void InitializeEnvironmentAndDatasources()
+        {
+            try
+            {
+                test = extent.CreateTest("InitializeEnvironmentAndDatasources").Info("Test Started");
+                DataLib.PopulateInCollection(@"C:\Users\User\Desktop\New folder\Interview\Interview.Test2\DataSources\Data.xlsx");
+                PropCollection.driver = new ChromeDriver();
+                PropCollection.driver.Manage().Window.Maximize();
+                test.Log(Status.Info,"Chrome Browser Launched Successfully");
+                PropCollection.driver.Navigate().GoToUrl("http://127.0.0.1:16864/index.html"); //replace with instance   
+                test.Log(Status.Info, "Navigated to Test Website Successfully");
+                test.Log(Status.Pass, "Intitialization of Environment and Datasources Passed");
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        [TearDown]
+        public void TcCleanup()
+        {
+            test = extent.CreateTest("TcCleanup").Info("Test Started");
+            PropCollection.driver.Close();
+            test.Log(Status.Pass, "Tear Down Successfull");
+        }
+
+    }
+}
--- Interview.Test2/TestSuite.cs
+++ Interview.Test2/TestSuite.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -11,42 +13,51 @@ using OpenQA.Selenium.Chrome;
 namespace Interview.Test2
 {
     [TestFixture]
-    class TestSuite
-    {
+    public class TestSuite : BaseTest
+    {static void Main(string[] args){}
 
-        static void Main(string[] args)
-        {                             }
-
-        [SetUp]
-        public void InitializeEnvironmentAndDatasources()
-        {
-            DataLib.PopulateInCollection(@"C:\Users\User\Desktop\New folder\Interview\Interview.Test2\DataSources\Data.xlsx");
-            PropCollection.driver = new ChromeDriver();
-            PropCollection.driver.Manage().Window.Maximize();
-            PropCollection.driver.Navigate().GoToUrl("http://127.0.0.1:16864/index.html"); //replace with instance
-            Console.WriteLine("Opened Browser");
-        }
-
-
+        
 
         [Test, Order(1)]
+        [Author("Austin Ryan Govender", "austinryang1@gmai.com")]
+        [Description("This test is to simulate Logging in to the application via via credentials parsed in Excel Data Source")]
         public void tc1VerifyLoginProcedurePositeTest()
         {
-            LoginPageObject loginObject = new LoginPageObject();
-            UpdatePageObjects pageObj = loginObject.Login(DataLib.ReadData(1, "Email"), DataLib.ReadData(1, "Password"));
+            try {
+                test = extent.CreateTest("tc1VerifyLoginProcedurePositeTest").Info("tc1VerifyLoginProcedurePositeTest Started");
+                LoginPageObject loginObject = new LoginPageObject();
+                UpdatePageObjects pageObj = loginObject.Login(DataLib.ReadData(1, "Email"), DataLib.ReadData(1, "Password"));
+                test.Log(Status.Info, "Successfully Logged In with DS Data");
+                test.Log(Status.Pass, "tc1VerifyLoginProcedurePositeTest Passed");
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
         }         
 
         [Test, Order(2)]
         public void tc2UpdateUserCredentialsUponLoginDDTPositiveTest()
         {
             //Testing cycle --dont forget to point to ExcelDataTableCount
-            for(int i =1; i <6 ; i++) { 
-            LoginPageObject loginObject = new LoginPageObject();
+            test = extent.CreateTest("tc1VerifyLoginProcedurePositeTest").Info("tc1VerifyLoginProcedurePositeTest Started");
+            try
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    LoginPageObject loginObject = new LoginPageObject();
 
-            UpdatePageObjects pageObj = loginObject.Login(DataLib.ReadData(i,"Email"), DataLib.ReadData(i, "Password"));
-
-            pageObj.UpdateDetails(DataLib.ReadData(i,"Initial"), DataLib.ReadData(i, "Name"), DataLib.ReadData(i, "Surname"), 
-                DataLib.ReadData(1, "UpdateEmail"), DataLib.ReadData(1, "UpdatePassword"));
+                    UpdatePageObjects pageObj = loginObject.Login(DataLib.ReadData(i, "Email"), DataLib.ReadData(i, "Password"));
+                    test.Log(Status.Pass, "Login Passed for Line: " + i );
+                    pageObj.UpdateDetails(DataLib.ReadData(i, "Initial"), DataLib.ReadData(i, "Name"), DataLib.ReadData(i, "Surname"),
+                        DataLib.ReadData(i, "UpdateEmail"), DataLib.ReadData(i, "UpdatePassword"));
+                    test.Log(Status.Pass, "Update Passed for: " + i);
+                }
+            }
+            catch (Exception e)
+            {
+                test.Log(Status.Fail, e.ToString());
+                throw;
             }
         }
 
@@ -61,13 +72,6 @@ namespace Interview.Test2
         {
             LoginPageObject loginObject = new LoginPageObject();
             loginObject.VerifyHyperLinkGithub();
-        }
-
-        [TearDown]
-        public void TcCleanup()
-        {
-            PropCollection.driver.Close();
-            Console.WriteLine("Closed the Browser");
         }
     }
 }
